9584bad1f477c8dbbc3e2c56cc38d0d3bb6ffae7
NicolaDiQuinzio <118384442+NicolaDiQuinzio@users.noreply.github.com>
2023-06-13 23:18:38 +0200

    commit

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use App\Models\Category;
+use App\Models\Tag;
+use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdatePostRequest;
+use Doctrine\DBAL\Schema\View;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
+
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $posts = Post::paginate(5);
+        return view('admin.posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Category::all();
+        $tags = Tag::all();
+        return view('admin.posts.create', compact('categories', 'tags'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorePostRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePostRequest $request)
+    {
+        $data = $request->validated();
+        $slug = Str::slug($request->title, '-');
+        $data['slug'] = $slug;
+        $data['user_id'] = Auth::id();
+        if ($request->hasFile('image')) {
+            $image_path = Storage::put('uploads', $request->image);
+            $data['image'] = asset('storage/' . $image_path);
+        }
+
+        $post = Post::create($data);
+
+
+        if ($request->has('tags')) {
+            $post->tags()->attach($request->tags);
+        }
+        return redirect()->route('admin.posts.show', $post->slug);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        return view('admin.posts.show', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        $categories = Category::all();
+        $tags = Tag::all();
+        return view('admin.posts.edit', compact('post', 'categories', 'tags'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatePostRequest  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatePostRequest $request, Post $post)
+    {
+        $data = $request->validated();
+        $slug = Str::slug($request->title, '-');
+        $data['slug'] = $slug;
+        if($request->hasFile('image')){
+            if($post->image){
+                 Storage::delete($post->image);
+            }
+
+            $image_path = Storage::put('uploads', $request->image);
+            $data['image'] = asset('storage/' . $image_path);
+      }
+
+        $post->update($data);
+        if ($request->has('tags')) {
+            $post->tags()->sync($request->tags);
+        } else {
+            $post->tags()->sync([]);
+        }
+        return redirect()->route('admin.posts.show', $post->slug)->with('message');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     *
+     */
+    public function destroy(Post $post)
+    {
+        if($post->image){
+           $togliere = "http://127.0.0.1:8000/storage/";
+          $image_remove =  str_replace($togliere , '' , $post->image);
+            Storage::delete($image_remove);
+        }
+        $post->delete();
+        return redirect()->route('admin.posts.index')->with('message', "$post->title deleted successfully.");
+    }
+}
---
+++ config/boolpress.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'posts' => [
+        [
+            "image" => "https://immagini.editorialedomani.it/version/c:ZDEzODQyM2QtNDhiZS00:ZWRhZTlj/pag_8_fat_peolple_don-t_go_to_heaven-def-jpg.webp?f=3%3A2&q=0.75&w=635",
+            "title" => "Il paradiso è dei magri",
+            "body" => "A partire da radici antiche la condanna della gola ha avuto una particolare rilevanza nella definizione della buona cristiana e del buon cristiano.<br>Tale condanna si ripete ancora oggi, attraverso l’ossessione per il corpo magro, la colpevolizzazione dell’obesità o anche del semplice sovrappeso.<br>Un punto di osservazione giudicante mette malamente in discussione le persone che soffrono di disturbi alimentari e racconta una storia che ancora oggi fa risuonare la propria eco, soprattutto nelle comunità statunitensi.",
+            "url" => "https://github.com/Gaetano26/coding-challenge"
+        ],
+
+
+    ]
+];
